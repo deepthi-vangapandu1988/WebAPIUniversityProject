f4d85105a83ea128d416c88b83b4ed95ec8a2e3a
Venkat <venkat.chikkanti@live.in>
2022-10-14 16:32:06 +0530

    Consuming APIs

---
+++ ConsumingAPI/WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            APIResponse result = new APIResponse();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/api/Home");
+
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMSIsInJvbGUiOiJTdHVkZW50IiwibmJmIjoxNjY1NzQxNzczLCJleHAiOjE2NjU3NzA1NzIsImlhdCI6MTY2NTc0MTc3M30.WweOvQXhVxusKIJgDgOijcVhOdFxzmGKxCckFslukFU");
+
+            HttpResponseMessage response = client.GetAsync("").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var data = await response.Content.ReadAsStringAsync();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                result = JsonConvert.DeserializeObject<APIResponse>(data);
+            }
+
+            return View(result.Data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConsumingAPI/WebApplication6/Models/APIResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace WebApplication6.Models
+{
+    public class APIResponse
+    {
+        public APIResponse()
+        {
+
+        }
+        public APIResponse(HttpStatusCode statusCode, bool isSuccess, List<StudentDTO> data = null, List<string> errors = null)
+        {
+            StatusCode = statusCode;
+            IsSuccess = isSuccess;
+            Data = data;
+            Errors = errors;
+        }
+
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+        public bool IsSuccess { get; set; } = true;
+        public List<StudentDTO> Data { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
